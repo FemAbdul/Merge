a299a1361196e262d080f51fa7385fa0dbd4c1b2
FemAbdul <95398555+FemAbdul@users.noreply.github.com>
2025-03-14 09:02:42 +0400

    Delete transcendence directory

--- transcendence/game/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_migrate
-from django.dispatch import receiver
-from .models import Game
-
-@receiver(post_migrate)
-def add_default_games(sender, **kwargs):
-    games = ["Pong", "Tic-Tac-Toe","Rock-Paper-Scissor"]
-    for game_name in games:
-        Game.objects.get_or_create(name=game_name)
